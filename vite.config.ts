f67dbed9b640b7c896660143d878172dea317b4d
sanjabh1103 <sanjabh1103@gmail.com>
2026-04-12 13:45:29 +0530

    fix: Add @turf/turf to rollup externals for dynamic import

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,9 @@ export default defineConfig(({ mode }) => ({
     },
     dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
   },
+  build: {
+    rollupOptions: {
+      external: ['@turf/turf'],
+    },
+  },
 }));
